19dcac59384c01da6614c4d1016312b569470ddc
Nasakareal <naasaka@outlook.es>
2025-09-21 21:50:41 -0600

    modelo vehiculo bien

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -9,6 +9,29 @@ class Vehicle extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'user_id',
+        'vehicle_type',
+        'transit_route_id',
+        'last_lat',
+        'last_lng',
+        'last_bearing',
+        'last_speed_kph',
+        'last_located_at',
+        'brand',
+        'model',
+        'color',
+        'plate_number',
+    ];
+
+    protected $casts = [
+        'last_lat'        => 'decimal:7',
+        'last_lng'        => 'decimal:7',
+        'last_bearing'    => 'integer',
+        'last_speed_kph'  => 'integer',
+        'last_located_at' => 'datetime',
+    ];
+
     public function driverAssignments()
     {
         return $this->hasMany(\App\Models\DriverVehicleAssignment::class, 'vehicle_id');
@@ -18,13 +41,14 @@ class Vehicle extends Model
     {
         return $this->hasOne(\App\Models\DriverVehicleAssignment::class, 'vehicle_id')->where('active', 1)->whereNull('ended_at');
     }
+
     public function routeAssignments()
     {
         return $this->hasMany(\App\Models\RouteVehicleAssignment::class, 'vehicle_id');
     }
+
     public function activeRouteAssignment()
     {
         return $this->hasOne(\App\Models\RouteVehicleAssignment::class, 'vehicle_id')->where('active', 1)->whereNull('ended_at');
     }
-
 }
